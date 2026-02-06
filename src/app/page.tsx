92bfe7b118067bd97fa888bb84851a4ec904c77e
colin357 <colin@ownitsocial.com>
2026-02-06 05:25:40 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
             <div className="relative">
               <div className="relative h-[500px] lg:h-[600px]">
                 <Image
-                  src="/images/headshot.jpg"
+                  src="/images/headshot.webp"
                   alt="Miami luxury real estate"
                   fill
                   className="object-cover"
